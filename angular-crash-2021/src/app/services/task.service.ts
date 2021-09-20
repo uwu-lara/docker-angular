391313b6aa9fd6d8fcd27fd6f3d80d23800b9edb
uwu-lara <ula.6364@gmail.com>
2021-09-20 11:14:10 +0530

    dockerize the production

--- angular-crash-2021/src/app/services/task.service.ts
+++ angular-crash-2021/src/app/services/task.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Task } from '../Task';
+import { environment } from './../../environments/environment';
 
 const httpOptions = {
   headers: new HttpHeaders({
@@ -13,9 +14,10 @@ const httpOptions = {
   providedIn: 'root',
 })
 export class TaskService {
-  private apiUrl = 'http://localhost:5000/tasks';
+  // private apiUrl = 'http://localhost:5000/tasks';
+  private apiUrl = environment.apiUrl;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   getTasks(): Observable<Task[]> {
     return this.http.get<Task[]>(this.apiUrl);
